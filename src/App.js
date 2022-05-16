d5d191bef559cde167969ca2a5195ee9e2cb7e9c
AswinCoddle <aswin.m@coddletech.com>
2022-05-16 15:08:38 +0530

    Frontend changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from React</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +16,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello world
         </a>
       </header>
     </div>
